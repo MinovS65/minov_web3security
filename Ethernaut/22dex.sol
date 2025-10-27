9ab28fcaff2202a639a5a9d21e3b392a2b894321
MinovS65 <pesopesaka@gmail.com>
2025-10-27 20:27:54 +0200

    Update 22dex.sol

--- Ethernaut/22dex.sol
+++ Ethernaut/22dex.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /*import "openzeppelin-contracts-08/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts-08/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts-08/access/Ownable.sol";
+import "openzeppelin-contracts-08/access/Ownable.sol";*/
 
 contract Hack {
     IDex dex;
