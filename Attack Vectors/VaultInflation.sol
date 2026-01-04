4c9d3d8bd2154e738fb9c23b4249233ae8e2df8a
MinovS65 <pesopesaka@gmail.com>
2026-01-04 14:22:34 +0200

    add foundry

--- Attack Vectors/VaultInflation.sol
+++ Attack Vectors/VaultInflation.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+import "forge-std/src/Test.sol";
 
 interface IERC20 {
     function totalSupply() external view returns(uint);
@@ -50,3 +51,6 @@ contract Vault {
     }
 }
 
+contract AttackPOC is Test {
+    
+}
