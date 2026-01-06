0610f80999948d1e7317b498a4624fcaaef125ce
MinovS65 <pesopesaka@gmail.com>
2026-01-06 23:53:18 +0200

    create

---
+++ FirstFlights54-Token0x/OverflowIn_transfer.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "forge-std/src/Test.sol";
+
+contract AttackPOC is Test {
+
+    function setUp () public {
+        
+    }
+}
